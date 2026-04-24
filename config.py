e473b39547766e21b7f66b6f31cc3c16d9840226
Yae Miko <aloksinghkumar756@gmail.com>
2026-04-24 13:57:17 +0530

    Add config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+MONGO_URI = os.environ.get(
+    "MONGO_URI",
+    "mongodb+srv://Payal:Aloksingh@payal.jv2kwch.mongodb.net/?appName=Payal",
+)
+DB_NAME = os.environ.get("DATABASE_NAME", "Anything")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+APP_ID = int(os.environ.get("APP_ID", "28614709"))
+API_HASH = os.environ.get("API_HASH", "f36fd2ee6e3d3a17c4d244ff6dc1bac8")
+
+OWNER = os.environ.get("OWNER", "Anythingbutnew56")
+OWNER_ID = int(os.environ.get("OWNER_ID", "8229041976"))
