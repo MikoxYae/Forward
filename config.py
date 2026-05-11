626059a736d0ea2c092ac8f9cea2cd2e57c8d31b
Yae Miko <aloksinghkumar756@gmail.com>
2026-05-11 12:01:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
 import os
 
-MONGO_URI = os.environ.get(
-    "MONGO_URI",
-    "mongodb+srv://Payal:Aloksingh@payal.jv2kwch.mongodb.net/?appName=Payal",
-)
-DB_NAME = os.environ.get("DATABASE_NAME", "Anything")
-
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8600454242:AAEDXLgf_zL3I7GfELzyJMru8lPRDLjwpPE")
 
 APP_ID = int(os.environ.get("APP_ID", "28614709"))
@@ -14,18 +8,14 @@ API_HASH = os.environ.get("API_HASH", "f36fd2ee6e3d3a17c4d244ff6dc1bac8")
 OWNER = os.environ.get("OWNER", "Anythingbutnew56")
 OWNER_ID = int(os.environ.get("OWNER_ID", "8229041976"))
 
-START_PIC = os.environ.get(
-    "START_PIC",
-    "https://graph.org/file/b4864a63946e9b1e84238-ccb51f7ec7e7c11458.jpg",
-)
+MONGO_URI = os.environ.get("MONGO_URI","mongodb+srv://Forward:Aloksingh@forward.lkv0fv3.mongodb.net/?appName=Forward",)
+DB_NAME = os.environ.get("DATABASE_NAME", "Anything")
+
+START_PIC = os.environ.get("START_PIC","https://graph.org/file/b4864a63946e9b1e84238-ccb51f7ec7e7c11458.jpg",)
 
 ACCEPT_DELAY = float(os.environ.get("ACCEPT_DELAY", "0"))
 
-DEFAULT_WELCOME = os.environ.get(
-    "DEFAULT_WELCOME",
-    (
+DEFAULT_WELCOME = os.environ.get("DEFAULT_WELCOME",(
         "<b>ʜᴇʟʟᴏ {mention} 👋</b>\n\n"
         "<b>ʏᴏᴜʀ ᴊᴏɪɴ ʀᴇǫᴜᴇsᴛ ғᴏʀ <a href=\"{chat_link}\">{chat_title}</a> ʜᴀs ʙᴇᴇɴ ᴀᴄᴄᴇᴘᴛᴇᴅ.</b>\n"
-        "<b>ᴡᴇʟᴄᴏᴍᴇ ᴀʙᴏᴀʀᴅ!</b>"
-    ),
-)
+        "<b>ᴡᴇʟᴄᴏᴍᴇ ᴀʙᴏᴀʀᴅ!</b>"),)
